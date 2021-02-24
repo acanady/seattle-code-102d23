bb0134816ca6c988b2e6e018cb5f1cfdd20fa18b
Roger Huba <rogerhuba@gmail.com>
2021-02-24 17:16:44 -0600

    add demo and code-along

---
+++ class-06/demo/app1.js
@@ -0,0 +1,3 @@
+let userName = prompt('Please Enter your Name: ');
+
+document.write('<h2>' + userName + '</h2>')
---
+++ class-06/demo/app2.js
@@ -0,0 +1,5 @@
+if (userName == 'Roger'){
+    document.write('<h3>' + 'Welcome Darth Roger' + '</h3>');
+} else {
+    document.write('<h3>' + 'Welcome ' + userName + '</h3>');
+}
