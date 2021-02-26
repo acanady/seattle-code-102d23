6a91f852ef937e18928b191857aa38e38cc3d8a5
mnfmnfm <mnfmnfm@gmail.com>
2021-02-26 13:41:04 -0800

    day 10 review

---
+++ class-10/app.js
@@ -0,0 +1,9 @@
+document.write('<p>js is connected!</p>');
+
+// num is the parameter for this function
+function addTwo(num) {
+  return num + 2;
+}
+//               7 is the argument I'm passing in this time
+document.write("<p>7 + 2 is " + addTwo(7) + "</p>");
+
